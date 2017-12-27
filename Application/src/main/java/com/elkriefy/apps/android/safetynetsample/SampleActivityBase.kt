de03e128df7c0e4ba37b78962b16854b80505d41
Yossi Elkrief <elkriefy@gmail.com>
2017-12-27 15:32:22 +0200

    Verify apps functions.

--- Application/src/main/java/com/elkriefy/apps/android/safetynetsample/SampleActivityBase.kt
+++ Application/src/main/java/com/elkriefy/apps/android/safetynetsample/SampleActivityBase.kt
@@ -5,6 +5,11 @@ import android.support.annotation.ColorRes
 import android.support.annotation.DimenRes
 import android.support.annotation.IntegerRes
 import android.support.v4.app.FragmentActivity
+import android.util.Log
+import com.google.android.gms.safetynet.SafetyNet
+import com.google.android.gms.safetynet.SafetyNetApi.HarmfulAppsResponse
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 
 
 /**
@@ -38,4 +43,78 @@ open class SampleActivityBase : FragmentActivity() {
     fun integer(@IntegerRes resId: Int): Int {
         return resources.getInteger(resId)
     }
+
+
+    fun listHarmfulApps() {
+        val listAppsTask: Task<HarmfulAppsResponse> = SafetyNet.getClient(this)
+                .listHarmfulApps()
+
+        listAppsTask.addOnCompleteListener(object : OnCompleteListener<HarmfulAppsResponse> {
+            override fun onComplete(task: Task<HarmfulAppsResponse>) {
+                Log.d(TAG, "Received listHarmfulApps() result")
+
+                if (task.isSuccessful()) {
+                    val result = task.getResult()
+                    val scanTimeMs = result.getLastScanTimeMs()
+                    val appList = result.getHarmfulAppsList()
+                    if (appList.isEmpty()) {
+                        Log.d(TAG, "There are no known " + "potentially harmful apps installed.")
+                    } else {
+                        Log.e(TAG, "Potentially harmful apps are installed!")
+                        for (harmfulApp in appList) {
+                            Log.e(TAG, "Information about a harmful app:")
+                            Log.e(TAG, "  APK: " + harmfulApp.apkPackageName)
+                            Log.e(TAG, "  SHA-256: " + harmfulApp.apkSha256)
+                            // Categories are defined in VerifyAppsConstants.
+                            Log.e(TAG, "  Category: " + harmfulApp.apkCategory)
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "An error occurred. " +
+                            "Call isVerifyAppsEnabled() to ensure " +
+                            "that the user has consented.")
+                }
+            }
+        })
+    }
+
+
+    fun isVerifyAppsEnabled() {
+        val verifyAppsEnabled = SafetyNet.getClient(this)
+                .isVerifyAppsEnabled
+
+        verifyAppsEnabled.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val result = task.result
+                if (result.isVerifyAppsEnabled) {
+                    Log.d(TAG, "The Verify Apps feature is enabled.")
+                } else {
+                    Log.d(TAG, "The Verify Apps feature is disabled.")
+                }
+            } else {
+                Log.e(TAG, "A general error occurred.")
+            }
+        }
+
+    }
+
+    fun enableVerifyApps() {
+        val enableVerifyApps = SafetyNet.getClient(this)
+                .enableVerifyApps()
+        enableVerifyApps.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val result = task.result
+                if (result.isVerifyAppsEnabled) {
+                    Log.d(TAG, "The user gave consent " + "to enable the Verify Apps feature.")
+                } else {
+                    Log.d(TAG, "The user didn't give consent " + "to enable the Verify Apps feature.")
+                }
+            } else {
+                Log.e(TAG, "A general error occurred.")
+            }
+        }
+    }
+
+
+
 }
